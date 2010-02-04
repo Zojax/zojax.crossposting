7dc3100c9c184f68aa75772a4b5cf4b8c642437d
Anatoly Bubenkov <bubenkoff@gmail.com>
2010-02-04 15:23:59 +0000

    headers check fix

--- src/zojax/crossposting/headers.py
+++ src/zojax/crossposting/headers.py
@@ -2,7 +2,4 @@ from zojax.resourcepackage.library import includes
 
 
 class CrosspostingHeaders(object):
-    
-    def isAvailable(self):
-        return 'zojax-crossposing' in includes.libraries
-        
+    pass    
